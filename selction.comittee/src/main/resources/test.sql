a119d008861b27cbca91939ccc9c3d510b537c99
MaksimAliakhnovich <maksim.aliakhnovich@gmail.com>
2018-10-14 17:40:32 +0300

    delete old version

--- selction.comittee/src/main/resources/test.sql
+++
@@ -1,3 +0,0 @@
-insert INTO faculty(faculty_name, recruitment_plan, subject_1_id, subject_2_id, subject_3_id) VALUES("FAIS", 150, 4, 5, 6);
-select * from faculty;
-select * from faculty WHERE faculty_id = 1;
